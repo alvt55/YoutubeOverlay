eb68aeab34541d93ec16346c7a4fd6a841d666d8
rho07 <rho07@students.cs.ubc.ca>
2024-10-06 02:38:43 -0700

    Floating Player + resizing implemented

--- backend/service-worker.js
+++ backend/service-worker.js
@@ -1,5 +1,6 @@
 
 
+//listen for clicks on Open Side Panel button
 chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
       id: 'openSidePanel',
@@ -7,6 +8,18 @@ chrome.runtime.onInstalled.addListener(() => {
       contexts: ['all']
     });
   });
+
+  
+  // Listen for clicks on the extension icon
+chrome.action.onClicked.addListener((tab) => {
+    chrome.sidePanel.isOpen((isOpen) => {
+        if (isOpen) {
+            chrome.sidePanel.close(); // Close if already open
+        } else {
+            chrome.sidePanel.open(); // Open the sidebar if closed
+        }
+    });
+});
   
   chrome.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === 'openSidePanel') {
